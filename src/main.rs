661fe986598348ca7e2de27fe70746a9f021bb7c
Marvin Gudel <github.marvin@gudel.org>
2023-10-05 16:04:48 +0200

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,58 @@
+use embedded_hal::spi::MODE_3;
+use esp_idf_hal::delay::Delay;
+use esp_idf_hal::gpio::{AnyIOPin, PinDriver};
+use esp_idf_hal::spi::{
+    config::{BitOrder, Config, DriverConfig},
+    SpiDeviceDriver,
+};
+use esp_idf_hal::units::FromValueType;
+use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
+use hcs_12ss59t::HCS12SS59T;
+use log::*;
+
+fn main() {
+    // It is necessary to call this function once. Otherwise some patches to the runtime
+    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+    esp_idf_sys::link_patches();
+    // Bind the log crate to the ESP Logging facilities
+    esp_idf_svc::log::EspLogger::initialize_default();
+
+    info!("Hello, world!");
+
+    let perip = esp_idf_hal::peripherals::Peripherals::take().unwrap();
+
+    let spi = perip.spi2;
+    let sclk = perip.pins.gpio3;
+    let data = perip.pins.gpio5;
+    let cs = perip.pins.gpio8;
+
+    let n_rst = PinDriver::output(perip.pins.gpio4).unwrap();
+    let n_vdon = PinDriver::output(perip.pins.gpio10).unwrap();
+
+    let spi_conf = Config::default()
+        .baudrate(3.MHz().into())
+        .bit_order(BitOrder::LsbFirst)
+        .data_mode(MODE_3);
+
+    let spi = SpiDeviceDriver::new_single(
+        spi,
+        sclk,
+        data,
+        Option::<AnyIOPin>::None,
+        Some(cs),
+        &DriverConfig::default(),
+        &spi_conf,
+    )
+    .unwrap();
+
+    let delay = Delay;
+
+    let mut vfd = HCS12SS59T::new(spi, n_rst, delay, Some(n_vdon));
+
+    vfd.init().unwrap();
+    vfd.display("Hello World!").unwrap();
+    info!("Should display \"Hello World!\" now.");
+    loop {
+        Delay::delay_ms(20);
+    }
+}
